d193737199747da2c94c02c062e983c38e398b75
Dumisani Ndume <ndumedumisani50@gmail.com>
2025-08-13 21:13:01 +0200

    Add files via upload

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -27,15 +27,22 @@ function showRandomQuote() {
   `;
 }
 
-const quotes = [
-  { text: "Quote 1 text", category: "Inspiration" },
-  { text: "Quote 2 text", category: "Motivation" },
-];
+const quotes = [];
+
+document.getElementById('add-quote-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const quoteText = document.getElementById('quote-text').value;
+  const quoteCategory = document.getElementById('quote-category').value;
+  addQuote(quoteText, quoteCategory);
+  document.getElementById('add-quote-form').reset();
+});
 
 function addQuote(text, category) {
   quotes.push({ text, category });
   console.log(`Added quote: "${text}" in category "${category}"`);
 }
+
+
 
 
 
