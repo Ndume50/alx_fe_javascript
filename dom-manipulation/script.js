44e20516d4ccce0c27f51fbb503f2b3c90d54850
Dumisani Ndume <ndumedumisani50@gmail.com>
2025-08-13 20:54:54 +0200

    Add files via upload

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -5,3 +5,10 @@ document.getElementById('add-content-btn').addEventListener('click', () => {
   contentItem.textContent = 'New Content Item';
   contentList.appendChild(contentItem);
 });
+
+const quotes = [
+  { text: "Quote 1 text", category: "Inspiration" },
+  { text: "Quote 2 text", category: "Motivation" },
+  { text: "Quote 3 text", category: "Inspiration" },
+];
+
